60f45c10656a89fd61e4c5cd107c7d2c9dd3b15a
sasa71928 <osalazarsalas@gmail.com>
2024-12-05 12:17:47 -0700

    links en logos y e imagen

--- PROYECTOFINAL/src/views/layouts/header.php
+++ PROYECTOFINAL/src/views/layouts/header.php
@@ -16,14 +16,31 @@ $user = isset($_SESSION['user']) ? $_SESSION['user'] : null;
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Protest+Strike&display=swap" rel="stylesheet">
     <title>VDC</title>
+    <style>
+        /* Estilo para eliminar la línea de subrayado */
+        .logo a, .vdc a {
+            text-decoration: none;
+            color: inherit; /* Mantiene el color actual del texto */
+            display: inline-block; /* Para que el área clicable abarque el contenido */
+        }
+        .vdc {
+            font-size: 24px;
+            font-weight: bold;
+        }
+        .logo a img {
+            display: block; /* Para que el área clicable abarque la imagen */
+        }
+    </style>
 </head>
 <body style="font-family: sans-serif;">
     <header>
         <div class="box logo">
-            <img class="imagen" src="<?= ASSETS_URL ?>/img/vinyl.png" alt="Vinyl">
+            <a href="<?=BASE_URL?>">
+                <img class="imagen" src="<?= ASSETS_URL ?>/img/vinyl.png" alt="Vinyl">
+            </a>
         </div>
         <div class="box vdc">
-            V.D.C.
+            <a href="<?=BASE_URL?>">V.D.C.</a>
         </div>
         <nav class="box nav">
             <a class="link" href="<?=BASE_URL?>">Inicio</a>
