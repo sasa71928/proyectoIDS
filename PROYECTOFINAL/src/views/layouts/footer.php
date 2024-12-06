69e0d51af0b8b04971bceaa119c12d91bbd512ac
AngelVital <111155035+AngelVital@users.noreply.github.com>
2024-12-06 03:47:59 -0700

    Update footer.php

--- PROYECTOFINAL/src/views/layouts/footer.php
+++ PROYECTOFINAL/src/views/layouts/footer.php
@@ -1,31 +1,35 @@
-<footer>
+<footer style="background-color: #222; color: #fff; padding: 40px 0;">
     <div class="container">
         <div class="row">
             <!-- Sobre nosotros -->
             <div class="col-md-4">
-                <h5>Sobre nosotros</h5>
+                <h5 style="color: #fff;">Sobre nosotros</h5>
                 <p>Somos una tienda especializada en vinilos, cassettes y CD's. Ofrecemos una amplia variedad de productos para los amantes de la música.</p>
             </div>
 
             <!-- Contacto -->
             <div class="col-md-4">
-                <h5>Contacto</h5>
-                <ul>
-                    <li>Correo electrónico: <a href="mailto:contacto@tienda.com">contacto@vdcshop.com</a></li>
-                    <li>Teléfono: 612 157 9811</li>
-                    <li>Dirección: La Paz, Baja California Sur</li>
+                <h5 style="color: #fff;">Contacto</h5>
+                <ul style="list-style-type: none; padding-left: 0;">
+                    <li><strong>Correo electrónico:</strong> <a href="mailto:contacto@vdcshop.com" style="color: #fff; text-decoration: none;">contacto@vdcshop.com</a></li>
+                    <li><strong>Teléfono:</strong> 612 157 9811</li>
+                    <li><strong>Dirección:</strong> La Paz, Baja California Sur</li>
                 </ul>
             </div>
 
             <!-- Redes sociales -->
             <div class="col-md-4">
-                <h5>Síguenos</h5>
-                <ul class="social-icons">
-                    <li><a href="https://facebook.com/tu_tienda" target="_blank"><i class="fab fa-facebook"></i> Facebook</a></li>
-                    <li><a href="https://twitter.com/tu_tienda" target="_blank"><i class="fab fa-twitter"></i> Twitter</a></li>
-                    <li><a href="https://instagram.com/tu_tienda" target="_blank"><i class="fab fa-instagram"></i> Instagram</a></li>
+                <h5 style="color: #fff;">Síguenos</h5>
+                <ul class="social-icons" style="list-style-type: none; padding-left: 0; display: flex; justify-content: space-around;">
+                    <li><a href="https://facebook.com/tu_tienda" target="_blank" style="color: #fff; font-size: 24px; text-decoration: none;"><i class="fab fa-facebook"></i> Facebook</a></li>
+                    <li><a href="https://twitter.com/tu_tienda" target="_blank" style="color: #fff; font-size: 24px; text-decoration: none;"><i class="fab fa-twitter"></i> Twitter</a></li>
+                    <li><a href="https://instagram.com/tu_tienda" target="_blank" style="color: #fff; font-size: 24px; text-decoration: none;"><i class="fab fa-instagram"></i> Instagram</a></li>
                 </ul>
             </div>
+        </div>
+
+        <div class="text-center" style="margin-top: 20px; font-size: 14px; color: #aaa;">
+            <p>&copy; 2024 VDC Shop. Todos los derechos reservados.</p>
         </div>
     </div>
 </footer>
